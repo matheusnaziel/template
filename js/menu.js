031793be6695b175e3cc80ac9d0b6f9ed3bcc54d
Matheus Naziel <matheus.naziel@gmail.com>
2017-08-14 21:05:24 -0300

    atualização-do-projeto

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,7 @@
+
 document.querySelector('.menu-abrir').onclick = function() {
     document.documentElement.classList.add('menu-ativo');
-    console.log("fui clicado")
+
 };
 
 document.querySelector('.menu-fechar').onclick = function() {
@@ -8,7 +9,7 @@ document.querySelector('.menu-fechar').onclick = function() {
 };
 
 document.documentElement.onclick = function(event) {
-    if (event.target === document.documentElement) {
-        document.documentElement.classList.remove('menu-ativo');
-    }
-};
+	if(event.target === document.documentElement) {
+		document.documentElement.classList.remove('menu-ativo');
+	}
+}
