963d5bc30f9614d72e98c73ea2fffd43c27e3c3e
Matheus Naziel <matheus.naziel@gmail.com>
2017-09-04 17:44:28 -0300

    itens do menu

--- js/menu.js
+++ js/menu.js
@@ -9,6 +9,20 @@ document.querySelector('.menu-fechar').onclick = function() {
     document.querySelector('.barra-nav').classList.remove('menu-ativo');
 };
 
+// jQuery(document).ready(function($) { 
+//     $(".scroll").click(function(event){ 
+//         event.preventDefault(); 
+// 		var valorHref = $(event.currentTarget).attr("href");
+		
+//     	var alturaElement = $(valorHref).offset().top
+
+
+//         $('.col-lg-10').animate({
+//         	scrollTop: alturaElement
+//         }, 600);
+//    });
+// });
+
 // document.documentElement.onclick = function(event) {
 // 	console.log('document.documentElement', blockClick)
 // 	if(blockClick && event.target.classList[1] === "menu-ativo") {
