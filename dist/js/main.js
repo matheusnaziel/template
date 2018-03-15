c6894dea68a87fd61b832f5cf308f2c99eea89c1
Matheus Naziel <matheus.naziel@gmail.com>
2018-03-15 14:38:52 -0300

    atualizacao do template

---
+++ dist/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	$('.toggle').click(function(){
+		$('.navbar-wrapper').toggleClass('active');
+		$(this).hide();
+	})
+	$('.nav-toggle').click(function(){
+		$('.navbar-wrapper').toggleClass('active');
+		$('.toggle').show();
+	})
+})
